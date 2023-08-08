24d9afb17719ef777cdedf47fa69c6b875ea2fba
Denise <deniselewis1120@gmail.com>
2023-08-07 19:29:29 -0700

    Display number of fetched quotes

--- src/components/quotes/Quotes.js
+++ src/components/quotes/Quotes.js
@@ -8,7 +8,7 @@ const Quotes = ({ quotes, categories, category, handleCategoryChange, addToFavor
             <div className="quotes wrapper">
                 <div className="category-header">
                     <h2>Pick your Favorite Quotes Below</h2>
-                    <p>Browse through your collection of quotes</p>
+                    <p>You have {quotes.length > 0 && "a collection of"} {quotes.length} great {category !== "All" && category} quote{quotes.length > 1 && "s"}!</p>
                     <CategoryForm categories={categories} category={category} handleCategoryChange={handleCategoryChange} />
                 </div>
                 {quotes.map(quote => <QuoteCard key={quote.id} quote={quote} addToFavorites={addToFavorites} favoriteQuotes={favoriteQuotes} />)}
