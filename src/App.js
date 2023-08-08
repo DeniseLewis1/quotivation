f2a117f7ab517b6ee3f503151c8716dfc5bea85a
Denise <deniselewis1120@gmail.com>
2023-08-07 18:58:19 -0700

    Display messages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { Loader } from "react-feather";
 import Quotes from "./components/quotes/Quotes";
 import FavoriteQuotes from "./components/quotes/FavoriteQuotes";
+import Message from "./components/Message";
 import "./App.css";
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
   const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState("All");
   const [favoriteQuotes, setFavoriteQuotes] = useState([]);
+  const [messageText, setMessageText] = useState("");
+  const [showMessage, setShowMessage] = useState(false);
   const quotesUrl = "https://gist.githubusercontent.com/skillcrush-curriculum/6365d193df80174943f6664c7c6dbadf/raw/1f1e06df2f4fc3c2ef4c30a3a4010149f270c0e0/quotes.js";
   const categories = ["All", "Leadership", "Empathy", "Motivation", "Learning", "Success", "Empowerment"];
   const maxFaves = 3;
@@ -40,12 +43,15 @@ function App() {
     const alreadyFavorite = favoriteQuotes.find(favorite => favorite.id === selectedQuote.id);
 
     if (alreadyFavorite) {
-      console.log("This quote is already in your favorites, choose another!");
+      setMessageText("This quote is already in your favorites, choose another!");
+      setShowMessage(true);
     } else if (favoriteQuotes.length < maxFaves) {
       setFavoriteQuotes([...favoriteQuotes, selectedQuote]);
-      console.log("Added to favorites!");
+      setMessageText("Added to favorites!");
+      setShowMessage(true);
     } else {
-      console.log("Max number of Favorite Quotes reached. Please delete one to add another!");
+      setMessageText("Max number of Favorite Quotes reached. Please delete one to add another!");
+      setShowMessage(true);
     }
   };
 
@@ -54,8 +60,11 @@ function App() {
     setFavoriteQuotes(updatedFavorites);
   };
 
+  const removeMessage = () => setShowMessage(false);
+
   return (
     <div className='App'>
+      {showMessage && <Message messageText={messageText} removeMessage={removeMessage} />}
       <Header />
       <main>
         <FavoriteQuotes favoriteQuotes={favoriteQuotes} maxFaves={maxFaves} removeFromFavorites={removeFromFavorites} />
