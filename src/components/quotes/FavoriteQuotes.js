a0f140e51b6fd103d383af0a8d24810b2618c2f1
Denise <deniselewis1120@gmail.com>
2023-08-08 09:30:44 -0700

    Improve text below favorite quotes

--- src/components/quotes/FavoriteQuotes.js
+++ src/components/quotes/FavoriteQuotes.js
@@ -2,13 +2,17 @@ import React from "react";
 import FavoriteQuoteCard from "./FavoriteQuoteCard";
 
 const FavoriteQuotes = ({ favoriteQuotes, maxFaves, removeFromFavorites }) => {
+    const remainingFavorites = maxFaves - favoriteQuotes.length;
+
     return (
         <section className="favorite-quotes">
             <div className="wrapper quotes">
                 <h3>Top 3 favorite quotes</h3>
                 {favoriteQuotes.length > 0 && <ul>{favoriteQuotes.map(quote => <FavoriteQuoteCard quote={quote} key={quote.id} removeFromFavorites={removeFromFavorites} />)}</ul>}
                 <div className="favorite-quotes-description">
-                    <p>You can add up to three favorites by selecting from the options below. <br /> Once you choose, they will appear here.</p>
+                    {favoriteQuotes.length < maxFaves && 
+                        <p>You can add {remainingFavorites} more quote{remainingFavorites > 1 &&"s"} to your top three favorites by selecting from the options below.
+                        <br /> Once you choose, they will appear here.</p>}
                 </div>
             </div>
         </section>
